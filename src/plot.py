f734ae2197955993868e164d87cf94eeed4d38ef
Chu Chu <chuc@example.com>
2021-04-25 01:19:40 +0800

    update plot.py

--- src/plot.py
+++ src/plot.py
@@ -8,14 +8,14 @@ label=["Size 8", "Size 16", "Size 32", "Size 64", "Size 128"]
 
 def plot_makespan(file_list, compare_num):
     makespan_list = []
-    runtime_list = []
+    runtime_list = [] 
     first_line = []
     lines = []
     for file in file_list:
-        with open(file, "r") as f:
-            first_line.append(f.readlines()[0])
-        with open(file, "r") as f:
-            lines.append(f.readlines()[1:])
+        with open(file, "r") as f: 
+            first_line.append(f.readlines()[0]) 
+        with open(file, "r") as f: 
+            lines.append(f.readlines()[1:])     
             
     for i, line in enumerate(lines):
         makespan = []
@@ -33,26 +33,26 @@ def plot_makespan(file_list, compare_num):
     for i in range(compare_num):
         #label.append((file_list[i].split("/")[4]).split(".")[0])
         ax.plot(runtime_list[i], makespan_list[i], 'o')
-#     x_major_locator = MultipleLocator(50)
-#     y_major_locator = MultipleLocator(500)
+    x_major_locator = MultipleLocator(50)
+    y_major_locator = MultipleLocator(500)
     ax = plt.gca()
     ax.xaxis.set_major_locator(x_major_locator)
     ax.yaxis.set_major_locator(y_major_locator)
     plt.tick_params(labelsize=12)
     plt.legend(label, loc='upper right', fontsize = 14)
-    fig.suptitle('Task Frequency = 1, Agent Number = 30', fontsize=16)
+    fig.suptitle('Task Frequency = 1, Agent Number = 50', fontsize=16)
     plt.xlabel('Runtime', fontsize=14)
     plt.ylabel('Makespan', fontsize=14)
-    plt.grid(linestyle='-.')
-    plt.rcParams['savefig.dpi'] = 300
-    plt.rcParams['figure.dpi'] = 300
+    plt.grid(linestyle='-.') 
+#     plt.rcParams['savefig.dpi'] = 300 
+#     plt.rcParams['figure.dpi'] = 300
     plt.show()
-    fig.savefig('../output/offline1/task-1-agent-30/neighbourhood_t1a30.png')
+    fig.savefig('../output/offline1/task-1-agent-50/neighbourhood_t1a50.png')
     
-f1 = "../output/offline1/task-1-agent-30/size-8.txt"
-f2 = "../output/offline1/task-1-agent-30/size-16.txt"
-f3 = "../output/offline1/task-1-agent-30/size-32.txt"
-f4 = "../output/offline1/task-1-agent-30/size-64.txt"
-f5 = "../output/offline1/task-1-agent-30/size-128.txt"
+f1 = "../output/offline1/task-1-agent-50/size-8.txt"
+f2 = "../output/offline1/task-1-agent-50/size-16.txt"
+f3 = "../output/offline1/task-1-agent-50/size-32.txt"
+f4 = "../output/offline1/task-1-agent-50/size-64.txt"
+f5 = "../output/offline1/task-1-agent-50/size-128.txt"
 file_list = [f1, f2, f3, f4, f5]
 plot_makespan(file_list, 5)
